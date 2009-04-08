4c45436346243ae898dba8536d34d30d69726e6a
Hernán Astudillo <naaano@gmail.com>
2009-04-08 12:56:22 -0400

    upgrade masivo

--- app/helpers/miembros_helper.rb
+++
@@ -1,9 +0,0 @@
-module MiembrosHelper
-  def options_for_association_conditions(association)
-    if association.name == :usuario
-      ['usuarios.id not in (select usuario_id from puestos where usuario_id is not null)' ]
-    else
-      super
-    end
-  end 
-end
