e2a4adaeebb76e9213b450554f07ec37f4bfc0cb
Hernán Astudillo <naaano@gmail.com>
2009-04-15 18:37:52 -0400

    seguridad en desarrollos y filtro activos

--- app/controllers/desarrollos_controller.rb
+++ app/controllers/desarrollos_controller.rb
@@ -1,23 +1,29 @@
 class DesarrollosController < ApplicationController
-  #uses_yui_editor
+  uses_yui_editor
   active_scaffold do |config|
-    config.list.columns =  [:id, :prioridad ,:titulo, :categoria, :status, :critico, :hh, :usuario]
+
+    config.actions.exclude :delete
+
+    config.list.columns =  [:id, :prioridad, :created_at ,:titulo, :categoria, :status, :critico, :hh, :usuario]
     config.create.columns = [ :titulo, :descripcion, :categoria, :critico]
     config.update.columns = [ :titulo, :descripcion, :categoria, :critico, :status, :prioridad, :hh]
     
     config.list.sorting = { :id => :desc }
-    
-    config.columns[:usuario].label = "ultimo cambio"
-    
+ 
     config.columns[:id].weight = 0
-    config.columns[:id].label = "ticket"
     config.columns[:id].css_class = "lalala"
     config.columns[:critico].form_ui = :checkbox
     config.columns[:status].form_ui = :select
     config.columns[:categoria].form_ui = :select
-    config.columns[:descripcion].form_ui = :textarea
-    config.columns[:descripcion].css_class = "rich_text_editor"
+    #config.columns[:descripcion].form_ui = :textarea
+    #config.columns[:descripcion].css_class = "rich_text_editor"
     #config.columns[:descripcion].options = {:cols => 70, :rows => 20}
+
+    config.columns[:status].clear_link
+    config.columns[:categoria].clear_link
+    config.columns[:usuario].clear_link
+
+    config.action_links.add 'Activos', :parameters => {:action => 'update_table',:status_id => [1,2,3]}, :page => true
   end
   
   def before_create_save(record)
@@ -28,6 +34,14 @@ class DesarrollosController < ApplicationController
   
   def before_update_save(record)
     record.usuario = current_user
+  end
+
+  def destroy_authorized?
+    false
+  end
+
+  def update_authorized?
+    return current_user.es_admin?
   end
   
   
